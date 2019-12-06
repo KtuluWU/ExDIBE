ee43d0cd4b2c562491a35f57972008c46b91e923
Yun <yun_wu@vip.126.com>
2019-12-06 13:02:28 +0100

    [Version3]
    Add a new python program for datas.

--- action.php
+++ action.php
@@ -14,7 +14,8 @@ $data = include 'config.php';
 $sftp_address = $data['sftp_address'];
 $sftp_login = $data['sftp_login'];
 $sftp_password = $data['sftp_password'];
-$str_python_param = $data['str_python_param'];
+$str_python_param_data = $data['str_python_param_data'];
+$str_python_param_pdf = $data['str_python_param_pdf'];
 
 class SFTPConnection
 {
@@ -68,13 +69,13 @@ class SFTPConnection
 
 @$siren = $_POST["siren"];
 $ident = $_POST["ident"];
-$pwd = $_POST["pwd"];
+@$pwd = $_POST["pwd"];
 $email = $_POST["email"];
 $commentaire = $_POST["commentaire"];
 @$python_option_entete = $_POST["python_option_entete"];
 @$python_option_ref = $_POST["python_option_ref"];
-//$format_res_pdf = $_POST["format_res_pdf"];
-//$format_res_data = $_POST["format_res_data"];
+$format_res_pdf = $_POST["format_res_pdf"];
+$format_res_data = $_POST["format_res_data"];
 @$file_upload = $_FILES["file_upload"];
 @$res_zip = $_POST["res_zip"];
 $flag = false;
@@ -84,7 +85,11 @@ $uploaded_filename_pre = explode('.', $file_upload["name"])[0];
 $uploaded_filename_url = str_replace(' ', '_', $uploaded_filename_pre);
 $uploaded_filename = str_replace(' ', '_', $file_upload["name"]);
 
-$str_python = $str_python_param . " -i " . $ident . " -p " . $pwd;
+
+$str_python_pdf = $str_python_param_pdf . " -i " . $ident . " -p " . $pwd;
+$str_python_data = $str_python_param_data . " -i " . $ident;
+
+// $str_python = $str_python_param . " -i " . $ident;
 // $str_python = "python ".$url_windows." -i ".$ident." -p ".$pwd;
 
 $v = ($file_upload ? true : false);
@@ -92,110 +97,135 @@ clean_up($siren, $v, $uploaded_filename_url);
 
 if ($siren && !$file_upload) {
     mkdir('./files/' . $siren, 0777, true);
-    $str_python .= " -s " . $siren . " -d ./files/" . $siren;
+    $str_python_pdf .= " -s " . $siren . " -d ./files/" . $siren;
+    $str_python_data .= " -s " . $siren . " -d ./files/" . $siren;
 } else if ($file_upload && !$siren) {
     mkdir('./files/' . $uploaded_filename_url, 0777, true);
     mkdir('./upload/' . $uploaded_filename_url, 0777, true);
     mkdir('./zip/' . $uploaded_filename_url, 0777, true);
     move_uploaded_file($file_upload["tmp_name"], "./upload/" . $uploaded_filename_url . "/" . $uploaded_filename);
-    $str_python .= " -f " . "./upload/" . $uploaded_filename_url . "/" . $uploaded_filename . " -d ./files/" . $uploaded_filename_url . "/";
+    $str_python_pdf .= " -f " . "./upload/" . $uploaded_filename_url . "/" . $uploaded_filename . " -d ./files/" . $uploaded_filename_url;
+    $str_python_data .= " -f " . "./upload/" . $uploaded_filename_url . "/" . $uploaded_filename . " -d ./files/" . $uploaded_filename_url;
 
     if ($python_option_entete == "true") {
-        $str_python .= " -e";
+        $str_python_pdf .= " -e";
+        $str_python_data .= " -e";
     }
 
     if ($python_option_ref == "true") {
-        $str_python .= " -r";
+        $str_python_pdf .= " -r";
+        $str_python_data .= " -r";
     }
 }
 
-exec($str_python, $output, $code);
-$files_siren = scandir("./files/" . $siren . "/");
-$files_multisirens = scandir("./files/" . $uploaded_filename_url . "/");
+$str_python_pdf.=" 2>&1";
+$str_python_data.=" 2>&1";
 
-if ($code == 0) {
-    $mail = new PHPMailer(true);
-    try {
-        $mail->CharSet = "UTF-8";
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'test.infogreffe@gmail.com';
-        $mail->Password = 'Infogreffe2019';
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
+//print($str_python);
+if ($format_res_pdf == "true" && $format_res_data == "false") {
+    exec($str_python_pdf, $output_pdf, $code_pdf);
+    $files_siren = scandir("./files/" . $siren . "/");
+    $files_multisirens = scandir("./files/" . $uploaded_filename_url . "/");
+    operation($code_pdf, $output_pdf, $file_upload, $siren, $files_siren, $files_multisirens, $uploaded_filename_url, $ident, $commentaire, $v, $format_res_pdf, $format_res_data, $sftp_address, $sftp_login, $sftp_password, $email, $res_zip);
+} else if ($format_res_pdf == "false" && $format_res_data == "true") {
+    exec($str_python_data, $output_data, $code_data);
+    $files_siren = scandir("./files/" . $siren . "/");
+    $files_multisirens = scandir("./files/" . $uploaded_filename_url . "/");
+    operation($code_data, $output_data, $file_upload, $siren, $files_siren, $files_multisirens, $uploaded_filename_url, $ident, $commentaire, $v, $format_res_pdf, $format_res_data, $sftp_address, $sftp_login, $sftp_password, $email, $res_zip);
+} else if ($format_res_pdf == "true" && $format_res_data == "true") {
+    exec($str_python_pdf, $output_pdf, $code_pdf);
+    exec($str_python_data, $output_data, $code_data);
+    $files_siren = scandir("./files/" . $siren . "/");
+    $files_multisirens = scandir("./files/" . $uploaded_filename_url . "/");
+    operation($code_pdf, $output_pdf, $file_upload, $siren, $files_siren, $files_multisirens, $uploaded_filename_url, $ident, $commentaire, $v, $format_res_pdf, $format_res_data, $sftp_address, $sftp_login, $sftp_password, $email, $res_zip);
+    operation($code_data, $output_data, $file_upload, $siren, $files_siren, $files_multisirens, $uploaded_filename_url, $ident, $commentaire, $v, $format_res_pdf, $format_res_data, $sftp_address, $sftp_login, $sftp_password, $email, $res_zip);
+}
 
-        $mail->setFrom('test.infogreffe@gmail.com', 'DataInfogreffe');
-        $mail->addAddress($email);
-        $mail->addCC('test.infogreffe@gmail.com');
+function operation($code, $output, $file_upload, $siren, $files_siren, $files_multisirens, $uploaded_filename_url, $ident, $commentaire, $v, $format_res_pdf, $format_res_data, $sftp_address, $sftp_login, $sftp_password, $email, $res_zip) {
+    if ($code == 0) {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->CharSet = "UTF-8";
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'test.infogreffe@gmail.com';
+            $mail->Password = 'Infogreffe2019';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
 
-        if (!$file_upload) {
-            foreach ($files_siren as $file) {
-                if ($file != '.' && $file != '..' && $file != '.DS_Store' && $file != '.gitkeep') {
-                    $mail->addAttachment("./files/" . $siren . "/" . $file);
-                }
-            }
-        } else {
-            foreach ($files_multisirens as $file) {
-                if (pathinfo($file, PATHINFO_EXTENSION) == 'csv') {
-                    $mail->addAttachment("./files/" . $uploaded_filename_url . "/" . $file);
-                }
-            }
-        }
+            $mail->setFrom('test.infogreffe@gmail.com', 'DataInfogreffe');
+            $mail->addAddress($email);
+            $mail->addCC('test.infogreffe@gmail.com');
 
-        if ($file_upload) {
-            foreach ($files_multisirens as $file) {
-                if (pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
-                    $zip = new ZipArchive();
-                    if ($res_zip != "") {
-                        $filename = $res_zip . ".zip";
-                    } else {
-                        $filename = $uploaded_filename_url . ".zip";
+            if (!$file_upload) {
+                foreach ($files_siren as $file) {
+                    if ($file != '.' && $file != '..' && $file != '.DS_Store' && $file != '.gitkeep') {
+                        $mail->addAttachment("./files/" . $siren . "/" . $file);
                     }
-                    $filename_compl = "./zip/" . $uploaded_filename_url . "/" . $filename;
-                    $zip->open($filename_compl, ZIPARCHIVE::CREATE);
-                    addFileToZip("./files/" . $uploaded_filename_url . "/", $zip);
-                    $zip->close();
                 }
-            }
-
-            if (!$filename_compl) {
-                echo "\n Erreur de format des donnnées du fichier (ou il n'y a aucun siren qui peut générer un pdf): " . $output . " (vient du Python)\n Essayez de supprimer les espaces dans le nom du fichier ou remplacez-les par « _ », et changez le format par csv (non UTF_8, séparé par virgule)\n";
             } else {
-                /**
-                 * Send zip to the server by sftp
-                 */
-                try
-                {
-                    $sftp = new SFTPConnection($sftp_address, 22);
-                    $sftp->login($sftp_login, $sftp_password);
-                    $sftp->uploadFile($filename_compl, "/home/rbe/" . $filename);
-                } catch (Exception $e) {
-                    echo "Erreur de SFTP: " . $e->getMessage() . "\n";
+                foreach ($files_multisirens as $file) {
+                    if (pathinfo($file, PATHINFO_EXTENSION) == 'xlsx' || pathinfo($file, PATHINFO_EXTENSION) == 'csv') {
+                        $mail->addAttachment("./files/" . $uploaded_filename_url . "/" . $file);
+                    }
                 }
             }
+
+            if ($file_upload && $format_res_pdf == "true") {
+                foreach ($files_multisirens as $file) {
+                    if (pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
+                        $zip = new ZipArchive();
+                        if ($res_zip != "") {
+                            $filename = $res_zip . ".zip";
+                        } else {
+                            $filename = $uploaded_filename_url . ".zip";
+                        }
+                        $filename_compl = "./zip/" . $uploaded_filename_url . "/" . $filename;
+                        $zip->open($filename_compl, ZIPARCHIVE::CREATE);
+                        addFileToZip("./files/" . $uploaded_filename_url . "/", $zip);
+                        $zip->close();
+                    }
+                }
+    
+                if (!$filename_compl) {
+                    echo "\n Erreur de format des donnnées du fichier (ou il n'y a aucun siren qui peut générer un pdf): " . $output . " (vient du Python)\n Essayez de changer le format par csv (non UTF_8, séparé par virgule)\n";
+                } else {
+                    /**
+                     * Send zip to the server by sftp
+                     */
+                    try
+                    {
+                        $sftp = new SFTPConnection($sftp_address, 22);
+                        $sftp->login($sftp_login, $sftp_password);
+                        $sftp->uploadFile($filename_compl, "/home/rbe/" . $filename);
+                    } catch (Exception $e) {
+                        echo "Erreur de SFTP: " . $e->getMessage() . "\n";
+                    }
+                }
+            }
+
+            $mail->isHTML(true);
+            $mail->Subject = 'ExDIBE - ' . $ident;
+            $mail->Body = email_body_html($ident, $commentaire);
+            $mail->AltBody = "IDENTIFIANT: " . $ident . " COMMENTAIRE: " . $commentaire;
+            $mail->send();
+
+            /**
+             *  Clean up
+             */
+            clean_up($siren, $v, $uploaded_filename_url);
+            /******************************/
+
+            echo "200";
+
+        } catch (Exception $e) {
+            
         }
-
-        $mail->isHTML(true);
-        $mail->Subject = 'ExDIBE - ' . $ident;
-        $mail->Body = email_body_html($ident, $commentaire);
-        $mail->AltBody = "IDENTIFIANT: " . $ident . " COMMENTAIRE: " . $commentaire;
-        $mail->send();
-
-        /**
-         *  Clean up
-         */
-        clean_up($siren, $v, $uploaded_filename_url);
-        /******************************/
-
-        echo "200";
-
-    } catch (Exception $e) {
-        echo $mail->ErrorInfo;
+    } else {
+        print_r("Erreur du python: ".$output);
     }
-} else {
-    echo "Erreur du programme Python.";
 }
 
 function addFileToZip($path, $zip)
